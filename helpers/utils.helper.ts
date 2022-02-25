0040c5b825d5923fc9a93e7f50c9064f19d7a764
João Carvalho <joao.carvalho@itcparts.com>
2022-02-25 10:16:38 +0000

    Refactored and added new methods.

--- helpers/utils.helper.ts
+++ helpers/utils.helper.ts
@@ -1,6 +1,8 @@
 import moment, { unitOfTime } from 'moment';
 import { createLogger, format, transports } from 'winston';
 
+import { control } from '../game-config.json';
+
 const logger = createLogger({
   level: 'info',
   format: format.printf((log) => `[${log.level.toUpperCase()}] - ${log.message}`),
@@ -8,57 +10,19 @@ const logger = createLogger({
   transports: [new transports.File({ filename: 'logs/log.txt' })]
 });
 
-/**
- * @description Returns a timestamp from the current date until the amount and type of time given.
- */
 export const addTime = (type: unitOfTime.DurationConstructor, amount: number) => moment().add(amount, type).format();
-
-/**
- * @description Returns the current date.
- */
 export const getDate = (date = new Date(), timeFormat = 'Do MMMM YYYY, h:mm:ss a') => moment(date).format(timeFormat);
-
-/**
- * @description Compare two dates.
- */
 export const compareDate = (firstDate: Date, secondDate: Date): boolean => moment(firstDate).isAfter(secondDate);
 
-/**
- * @description Transforms the given number string into a number.
- */
-export const getNumber = (amount: string) => {
-  const numberAmount = parseInt(amount, 10);
+const checkControl = (num: number) => num * control + (num - 1) * control;
+const checkAmount = (amount: number) => amount && amount > 0 && amount < 100 && !isNaN(amount);
 
-  if (numberAmount && numberAmount > 0 && numberAmount < 100 && !isNaN(numberAmount)) {
-    return numberAmount;
-  }
-};
-
+export const getNumber = (amount: string) => (checkAmount(parseInt(amount, 10)) ? parseInt(amount, 10) : undefined);
 export const getRandom = (max: number, min = 1) => Math.floor(Math.random() * (max - min + 1) + min);
+export const increment = (num: number, toInc: number) => (num >= checkControl(toInc) ? toInc + 1 : toInc);
 export const getBool = () => Math.random() < 0.5;
 
-export const getReason = (reason: string, prefix?: string) => {
-  if (!reason) {
-    reason = 'No reason provided';
-  }
-  if (prefix) {
-    reason = reason.replace(prefix, '');
-  }
+export const getReason = (reason: string) => (reason ? `Reason: ${reason}` : 'No reason provided');
 
-  return `Reason: ${reason}`;
-};
-
-/**
- * @description Logs an error entry.
- */
-export const logError = (error: any) => {
-  console.log(error);
-  logger.log('error', `${error.message}\n${error}\nTime: ${getDate()}`);
-};
-
-/**
- * @description Logs an information entry.
- */
-export const logInfo = (message: string) => {
-  logger.log('info', `${message}\nTime: ${getDate()}`);
-};
+export const logError = (error: any) => logger.log('error', `${error.message}\n${error}\nTime: ${getDate()}`);
+export const logInfo = (message: string) => logger.log('info', `${message}\nTime: ${getDate()}`);
